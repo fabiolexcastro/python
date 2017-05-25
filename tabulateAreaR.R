06626a29bd2ef22d89615d93efb4762e13afb823
fabiolexcastro <fabioalexandercastro@gmail.com>
2017-05-24 22:16:24 -0500

    Made a little change in the code,
     about the variable name

--- tabulateAreaR.R
+++ tabulateAreaR.R
@@ -40,7 +40,7 @@ tabArea <- function(path_raster, name_raster, name_field, shp_zone, out_table, n
 
 path  <- 'D:/directory/_data'
 files <- list.files(paste0(path, '/_raster'), full.names = T, pattern = '.tif$')
-basin <- shapefile(paste0(path, '/_shp/shapefile.shp.shp'))
+basin <- shapefile(paste0(path, '/_shp/shapefile.shp'))
 namesBasin <- as_data_frame(apply(basin@data,2,function(x)gsub('//s+', '',x)))$name_field
 basin <- mutate(basin, NameOk = substr(namesBasin, 1, 10))
 
